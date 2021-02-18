7ccab25df8ffb888e17617caad41c37ae48bd804
ztoms <tommyzhao111@gmail.com>
2021-02-18 09:27:40 -0500

    hotfix

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -1,43 +1,53 @@
 # -*- coding: utf-8 -*-
+import datetime
+from io import StringIO
 import streamlit as st
 import pandas as pd
 import pydeck as pdk
+import requests
 import urllib
 
 st.title("Park Visitation Sample Data Mapping")
 
 PARK_DATA = ('https://raw.githubusercontent.com/ztoms/Park-Visitation-Dashboard/main/data/parks_poi-part1.csv')
-PARK_PATTERNS_DATA = "https://raw.githubusercontent.com/ztoms/Park-Visitation-Dashboard/main/data/parks_patterns-part1.csv"
+PARK_PATTERNS_SAMPLE_DATA = "https://raw.githubusercontent.com/ztoms/Park-Visitation-Dashboard/main/data/parks_patterns-part1.csv"
+PARK_PATTERNS_2018_DATA = "https://psu.box.com/shared/static/ke90o1dxe4x3kx40fvgcxqplft3cctqc.csv"
+
 
 @st.cache
-def park_patterns_df():
-    return pd.read_csv(PARK_PATTERNS_DATA)
+def park_patterns_sample_df():
+    return pd.read_csv(PARK_PATTERNS_SAMPLE_DATA)
+
+@st.cache
+def park_patterns_2018_df():
+    data = pd.read_csv(PARK_PATTERNS_2018_DATA)
+    data['date_range_start'] = pd.to_datetime(data['date_range_start'], utc=True, errors='coerce') # convert to tz_naive
+    data['date_range_end'] = pd.to_datetime(data['date_range_end'], utc=True, errors='coerce')
+    return data
+
+@st.cache
+def group_patterns_by_date(df, start, end):
+    filtered_by_date = df[df['date_range_start'] > start and df['date_range_start'] < end]
+    sum_visits_in_range = filtered_by_date.groupby(['safegraph_place_id', 'location_name']).raw_visitor_counts.agg('sum')
+    return sum_visits_in_range
+
+@st.cache
+def filter_visitation_by_name(df, park_name):
+    filtered_by_name = df[df['location_name'] == park_name]
+    indexed_by_date = filtered_by_name.set_index('date_range_start')
+    return indexed_by_date
 
 @st.cache
 def park_poi_df():
-    return pd.read_csv(PARK_DATA).set_index('location_name')
+    data = pd.read_csv(PARK_DATA)
+    data = data.set_index('location_name')
+    return data
 
 
 poi_df = park_poi_df()
+#patterns_2018 = park_patterns_2018_df()
 
 try:
-    parks = st.multiselect("Choose park", list(poi_df.index))
-
-    st.sidebar.markdown('### Time Filters')
-    year = st.sidebar.selectbox('Year', ["2018","2019","2020"])
-    type = st.sidebar.selectbox('Type', ["By Week","By Month"])
-    time_range = st.sidebar.slider('Time Range', min_value=0, max_value=50)
-
-    data = park_patterns_df
-
-    #data['date_range_start'] = data['date_range_start'].astype('|S')
-    #data.dtypes
-    #selected_year = data[data['date_range_start'].str.contains(year)]
-    #data = data[['date_range_start', 'lng', 'lat', 'raw_visitor_counts']]
-    #st.line_chart(data)
-
-
-    parks = st.multiselect("Choose park", list(poi_df.index))
     ALL_LAYERS = {
         "Park Locations": pdk.Layer(
             "ScatterplotLayer",
@@ -49,7 +59,7 @@ try:
         ),
         "Parks Visited (9/28/2020 - 10/5/2020)": pdk.Layer(
             "ScatterplotLayer",
-            PARK_PATTERNS_DATA,
+            PARK_PATTERNS_SAMPLE_DATA,
             get_position=['lng', 'lat'],
             get_color=[200, 30, 0, 160],
             get_radius=['100 + raw_visitor_counts*4'],
@@ -86,6 +96,20 @@ try:
         ))
     else:
         st.error("Please choose at least one layer above.")
+        
+    st.sidebar.markdown('### Time Filters')
+    year = st.sidebar.selectbox('Year', ["2018","2019","2020"])
+    range_size = st.sidebar.selectbox('Type', ["By Week","By Month"])
+    start_date = st.date_input("Start date", datetime.datetime(2018, 1, 1))
+    end_date = st.date_input("End date")
+    month_selected = st.sidebar.slider('Time Slider', min_value=1, max_value=12)
+
+
+    park_selected = st.selectbox("Choose park", list(poi_df.index))
+        
+    # charting
+    #selected_park_df = filter_visitation_by_name(poi_df, park_selected)
+    #st.line_chart(selected_park_df[['raw_visitor_counts', 'raw_visit_counts']])
 
 except urllib.error.URLError as e:
     st.error("""
